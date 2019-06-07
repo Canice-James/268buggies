a56b7684cb3f697cc1855506fc3ba9a1ac1fc1a5
Shaquille Lynch <milokili111@gmail.com>
2019-06-06 20:47:39 -0400

    Finish all the queries in BuggyModel

--- models/BuggyModel.php
+++ models/BuggyModel.php
@@ -19,7 +19,17 @@ class BuggyModel
     $query = "INSERT INTO self::$table_name(Buggy_ID, Colour, Run_Duration, Run_Count, Run_Left)
     VALUES('". $id ."', '". $colour ."', '". $duration ."', '". $runCount ."', '". $runLeft ."')";
 
-    self::$connection->query($query);
+    $stmt = self::$connection->query($query);
+
+    $dataReport = "";
+
+    if($stmt === TRUE){
+      $dataReport = "Data Inserted Successfully";
+    }else{
+      $dataReport = "Data wasn't inserted successfully!! Error: " . $query . "<br>" . self::$connection->error;
+    }
+
+    return $dataReport;
   }
 
   //R
@@ -30,10 +40,36 @@ class BuggyModel
 
     return $stmt;
   }
+
   //U
-  public static function update()
-  { }
+  public static function update($id, $colour, $duration, $runCount, $runLeft){
+    $query = "UPDATE self::$table_name SET Colour = '".$colour."', Run_Duration = '".$duration."', Run_Count = '".$runCount."', Run_Left = '".$runLeft."' WHERE Buggy_ID = '".$id."';";
+    $stmt = self::$connection->query($query);
+
+    $dataReport = "";
+
+    if($stmt === TRUE){
+      $dataReport = "Data Updated Successfully";
+    }else{
+      $dataReport = "Data wasn't updated successfully!! Error: " . $query . "<br>" . self::$connection->error;
+    }
+
+    return $dataReport;
+  }
+
   //D
-  public static function delete()
-  { }
+  public static function delete($id){
+    $query = "DELETE FROM self::$table_name WHERE Buggy_ID = '".$id."';";
+    $stmt = self::$connection->query($query);
+
+    $dataReport = "";
+
+    if($stmt === TRUE){
+      $dataReport = "Data Deleted Successfully";
+    }else{
+      $dataReport = "Data wasn't deleted successfully!! Error: " . $query . "<br>" . self::$connection->error;
+    }
+
+    return $dataReport;
+  }
 }
