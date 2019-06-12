5314413f2421ac7a370afc8a7c158666dc42212e
Shaquille Lynch <milokili111@gmail.com>
2019-06-11 20:10:07 -0400

    Changes in the partModel php file

--- models/partModel.php
+++ models/partModel.php
@@ -15,9 +15,23 @@ class BuggyModel
   }
 
   //C
-  public static function create(){
-    
+  public static function create($partId, $partName, $supplierID, $unitPrice, $runRate, $orderDate, $quantity){
+    $query = "INSERT INTO self::$table_name(Part_ID, Part_Name, Supplier_ID, Unit_Price, Run_Rate, Order_Date, Quantity)
+    VALUES('". $partId ."', '". $partName ."', '". $supplierID ."', '". $unitPrice ."', '". $runRate ."', '". $orderDate ."', '". $quantity ."')";
+
+    $stmt = self::$connection->query($query);
+
+    $dataReport = "";
+
+    if($stmt === TRUE){
+      $dataReport = "Data Inserted Successfully";
+    }else{
+      $dataReport = "Data wasn't inserted successfully!! Error: " . $query . "<br>" . self::$connection->error;
+    }
+
+    return $dataReport;
   }
+
   //R
   public static function getAll()
   {
@@ -29,7 +43,20 @@ class BuggyModel
   //U
   public static function update()
   { }
+  
   //D
-  public static function delete()
-  { }
+  public static function delete($partId){
+    $query = "DELETE FROM self::$table_name WHERE Buggy_ID = '".$id."';";
+    $stmt = self::$connection->query($query);
+
+    $dataReport = "";
+
+    if($stmt === TRUE){
+      $dataReport = "Data Deleted Successfully";
+    }else{
+      $dataReport = "Data wasn't deleted successfully!! Error: " . $query . "<br>" . self::$connection->error;
+    }
+
+    return $dataReport;
+  }
 }
