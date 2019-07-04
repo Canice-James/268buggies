c1c1a299d0a5c5e6e77abca4ad1c79e1fd734b7a
unknown <micahgeorge99@gmail.com>
2019-07-04 15:57:48 -0400

    Group tours (group_members) added

--- models/tourModel.php
+++ models/tourModel.php
@@ -36,6 +36,26 @@ class TourModel
     return array("message"=> $message, "code"=>$code);
   }
 
+  public static function createGroup($member_id, $mem_fname, $mem_lname, $mem_address, $mem_phone, $mem_tour_date, $client_id, $tour_id){
+    $query = "INSERT INTO " . $group_member ."(member_id, mem_fname, mem_lname, mem_address, mem_phone, mem_tour_date, client_id, tour_id)
+    VALUES('". $member_id ."', '". $mem_fname ."', '". $mem_lname ."', '". $mem_address ."', '". $mem_phone ."', '". $mem_tour_date ."', ". $client_id .", '". $tour_id ."')";
+
+    $stmt = self::$connection->prepare($query);
+
+    $message = "";
+    $code = "";
+
+    if($stmt->execute() === TRUE){
+      $message = "Data Updated Successfully";
+      $code = 200;
+    }else{
+      $message = "Data wasn't updated successfully!! Error: " . $query . "<br>" . self::$connection->error;
+      $code = 500;
+    }
+
+    return array("message"=> $message, "code"=>$code);
+  }
+
   public static function getAll()
   {
     $query = 'SELECT * FROM ' . self::$table_name;
@@ -44,9 +64,26 @@ class TourModel
     return $stmt;
   }
 
+  
+  public static function getAllGroup()
+  {
+    $query = 'SELECT * FROM ' . $group_member;
+    $stmt = self::$connection->query($query);
+
+    return $stmt;
+  }
+
   public static function getOne($id)
   {
     $query = 'SELECT * FROM ' . self::$table_name . ' WHERE tour_id ="' . $id . '"';
+    $stmt = self::$connection->query($query);
+
+    return $stmt;
+  }
+
+  public static function getOneGroup($id)
+  {
+    $query = 'SELECT * FROM ' . $group_member . ' WHERE member_id ="' . $id . '"';
     $stmt = self::$connection->query($query);
 
     return $stmt;
@@ -70,9 +107,45 @@ class TourModel
     return array("message"=> $message, "code"=>$code);
    }
 
+   public static function updateGroup($mem_fname, $mem_lname, $mem_address, $mem_phone, $mem_tour_date, $client_id, $tour_id){
+    $query = "UPDATE " . $group_member . " SET mem_fname = '".$mem_fname."',' mem_lname = '".$mem_lname."', 'mem_address' = '".$mem_address."', 'mem_phone' = '".$mem_phone."', mem_tour_date = '.$mem_tour_date.', client_id = '".$client_id."', tour_id = '".$tour_id."';";
+    $stmt = self::$connection->prepare($query);
+
+    $message = "";
+    $code = "";
+
+    if($stmt->execute() === TRUE){
+      $message = "Data Updated Successfully";
+      $code = 200;
+    }else{
+      $message = "Data wasn't updated successfully!! Error: " . $query . "<br>" . self::$connection->error;
+      $code = 500;
+    }
+
+    return array("message"=> $message, "code"=>$code);
+   }
+
   //D
   public static function delete($tour_id){
     $query = "DELETE FROM " . self::$table_name ." WHERE tour_id = '".$tour_id."';";
+    $stmt = self::$connection->prepare($query);
+
+    $message = "";
+    $code = "";
+
+    if($stmt->execute() === TRUE){
+      $message = "Data Updated Successfully";
+      $code = 200;
+    }else{
+      $message = "Data wasn't updated successfully!! Error: " . $query . "<br>" . self::$connection->error;
+      $code = 500;
+    }
+
+    return array("message"=> $message, "code"=>$code);
+  }
+
+  public static function deleteGroup($member_id){
+    $query = "DELETE FROM " . $group_member ." WHERE member_id = '".$member_id."';";
     $stmt = self::$connection->prepare($query);
 
     $message = "";
