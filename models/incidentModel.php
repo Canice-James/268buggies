d1737663e9328fd39a1081d186e0a60144a48456
unknown <micahgeorge99@gmail.com>
2019-07-04 12:22:42 -0400

    incidents working fully

--- models/incidentModel.php
+++ models/incidentModel.php
@@ -1,4 +1,5 @@
 <?php
+
 class IncidentModel
 {
 
@@ -8,14 +9,13 @@ class IncidentModel
   // table name
   public static $table_name = "incident";
 
-  // function to handle errors saved as a variable
-
   public static function setConnection($connection)
   {
     self::$connection = $connection;
   }
 
-  public static function create($incid_id, $client_id, $incid_desc, $cause_of_incid, $recom, $date, $time)
+  //C
+ public static function create($incid_id, $client_id, $incid_desc, $cause_of_incid, $recom, $date, $time)
   { 
     $query = "INSERT INTO " . self::$table_name ."(incid_id, client_id, incid_desc, cause_of_incid, recom, date, time)
     VALUES('". $incid_id ."', '". $client_id ."', '". $incid_desc ."', '". $cause_of_incid ."', '". $recom ."', '". $date ."', '". $time ."')";
@@ -31,33 +31,35 @@ class IncidentModel
     }else{
       $message = "Data wasn't inserted successfully!! Error: " . $query . "<br>" . self::$connection->error;
       $code = 500;
+    }
+
+
+    self::printErrors();
+
+    $res = array("message"=> $message, "code"=>$code);
+    // var_dump( $res);
+    return $res;
   }
 
+  //R
   public static function getAll()
   {
     $query = 'SELECT * FROM ' . self::$table_name;
     $stmt = self::$connection->query($query);
-    
+
     return $stmt;
   }
 
-  public static function getOne($id)
+  public static function getOne($incid_id)
   {
     $query = 'SELECT * FROM ' . self::$table_name . 'WHERE incid_id =' . $id;
     $stmt = self::$connection->query($query);
-    
+
     return $stmt;
   }
 
-  public static function getMostRecentDate(){
-    $query = 'SELECT Date FROM ' . self::$table_name . ' LIMIT 1';
-    $stmt = self::$connection->prepare($query);
-    //self::printErrors();
-    $stmt->execute();
-    return $stmt->fetch()[0];
-  }
-
-  public static function update($client_id, $incid_desc, $cause_of_incid, $recom, $date, $time)
+  //U
+ public static function update($client_id, $incid_desc, $cause_of_incid, $recom, $date, $time)
   { 
     $query = "UPDATE ". self::$table_name ." SET client_id='".$client_id."', incid_desc='".$incid_desc."', cause_of_incid='".$cause_of_incid."', recom='".$recom."', date='".$date."', time='".$time."'";
     $stmt = self::$connection->prepare($query);
@@ -75,11 +77,11 @@ class IncidentModel
 
     return array("message"=> $message, "code"=>$code);
   }
-  }
+
   //D
-  public static function delete($incid_id)
+ public static function delete($incid_id)
   { 
-    $query = "DELETE FROM ". self::$table_name ." WHERE incid_id='".$incid_id."';";
+    $query = "DELETE FROM ". self::$table_name ." WHERE incid_id='".$incid_id."'";
     $stmt = self::$connection->prepare($query);
 
     $message = "";
@@ -97,8 +99,15 @@ class IncidentModel
     return $res;
   }
 
+  public static function getMostRecentDate(){
+    $query = 'SELECT Date FROM ' . self::$table_name . ' LIMIT 1';
+    $stmt = self::$connection->prepare($query);
+    //self::printErrors();
+    $stmt->execute();
+    return $stmt->fetch()[0];
+  }
+
   public static function printErrors(){
     var_dump(self::$connection->errorinfo(), TRUE);
   }
-  
 }
